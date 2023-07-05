89f99b856af7f6b4d3f426d35a74db19bd39d82b
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-05 16:35:21 +0500

    Done!

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -11,6 +11,6 @@ public class ReassigningValues {
         linkToFirst = 15;
         linkToSecond = 6;
         linkToThird = 4;
-        System.out.println("\n" + linkToFirst +" \n" + linkToSecond + "\n" + linkToThird + "\n\n" + first +" \n"  + second + "\n" +  third);
+        System.out.println(linkToFirst +" \n" + linkToSecond + "\n" + linkToThird + "\n" + first +" \n"  + second + "\n" +  third);
     }
 }
